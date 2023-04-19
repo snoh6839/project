c707445906f4197f297e1425785c689385e2c3c1
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-04-19 12:05:04 +0900

    update

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (!$db_conn) {
 
 // 전체 데이터 수 가져오기
 $total_data_count = $db_conn->query('SELECT COUNT(*) FROM Task')->fetchColumn();
-$page_data_count = 5; // 페이지당 보여줄 데이터 수
+$page_data_count = 8; // 페이지당 보여줄 데이터 수
 $total_page_count = ceil($total_data_count / $page_data_count);
 
 // 현재 페이지 번호 구하기
@@ -30,62 +30,89 @@ $task_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // HTML 페이지에 표시할 코드 작성
 ?>
-<!-- 데이터 출력 -->
-<div>
-    <table>
-        <thead>
-            <tr>
-                <th>글번호</th>
-                <th>날짜</th>
-                <th>카테고리</th>
-                <th>제목</th>
-                <th>체크박스</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($task_data as $data) { ?>
-                <tr>
-                    <!-- 데이터 출력 시 htmlspecialchars 함수를 사용하여 보안 이슈 방지
-		문장내에 HTML코드가 들어가는 특수문자를 포함시켜 입력하고 화면으로 출력할 때,
-		HTML의 특수문자가 HTML태그로 적용되는 것이아니라 일반 문자로 인식되어 그대로 출력되게 해주는 역할이다.
-		바꾸는 문자로는 예시로
-		&는 &amp;로 바꾼다. "는 &quot;로 바꾼다. '는 &#039;로 바꾼다. <는 &lt로 바꾼다. >는 &gt로 바꾼다.-->
-                    <td><?php echo htmlspecialchars($data['task_no']); ?></td>
-                    <td><?php echo htmlspecialchars($data['task_date']); ?></td>
-                    <td><?php echo htmlspecialchars($data['category_no']); ?></td>
-                    <td><?php echo htmlspecialchars($data['task_title']); ?></td>
-                    <td>
-                        <input type="checkbox" <?php echo $data['is_com'] == '1' ? 'checked' : ''; ?> onclick="if(this.checked){this.parentNode.parentNode.style.textDecoration='line-through';}else{this.parentNode.parentNode.style.textDecoration='none';}">
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-</div>
-<!-- 페이지네이션 출력 -->
-<ul class="paging-wrap">
+<!DOCTYPE html>
+<html lang="ko">
 
-    <?php
-    $prevPage = ($page == 1) ? $num_pages : $page - 1;
-    $nextPage = ($page == $num_pages) ? 1 : $page + 1;
-    if ($page > 1) {
-    ?>
-        <li><a href='index.php?page_num=<?php echo $prevPage; ?>'>Prev</a></li>
-    <?php } ?>
-    <?php
-    for ($i = 1; $i <= $num_pages; $i++) {
-        if ($i === (int)$page) {
-    ?>
-            <li><a href="/board/index.php?page_num=<?php echo $i ?>" class="page-icon active"><?php echo $i ?></a></li>
-        <?php
-        } else {
-        ?>
-            <li><a href="/board/index.php?page_num=<?php echo $i ?>" class="page-icon"><?php echo $i ?></a></li>
-        <?php
-        }
-    }
-    if ((int)$page < $num_pages) {
-        ?>
-        <li><a href='/board/index.php?page_num=<?php echo $nextPage; ?>'>Next</a></li>
-    <?php } ?>
-</ul>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>미라클 모닝</title>
+    <!-- css 링크 -->
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<!-- 데이터 출력 -->
+
+<body>
+    <div class="sidebox">
+        <div class="top"></div>
+        <div class="bottom"></div>
+    </div>
+    <div class="contianer">
+        <div class="title top">
+            <h1>MIRACLE MORNING</h1>
+        </div>
+        <div class="bottom">
+            <div class="listTable">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>글번호</th>
+                            <th>날짜</th>
+                            <th>카테고리</th>
+                            <th>제목</th>
+                            <th>수행여부</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($task_data as $data) { ?>
+                            <tr>
+                                <!-- 데이터 출력 시 htmlspecialchars 함수를 사용하여 보안 이슈 방지
+                                문장내에 HTML코드가 들어가는 특수문자를 포함시켜 입력하고 화면으로 출력할 때,
+                                HTML의 특수문자가 HTML태그로 적용되는 것이아니라 일반 문자로 인식되어 그대로 출력되게 해주는 역할이다.
+                                바꾸는 문자로는 예시로
+                                &는 &amp;로 바꾼다. "는 &quot;로 바꾼다. '는 &#039;로 바꾼다. <는 &lt로 바꾼다. >는 &gt로 바꾼다.-->
+                                <td><?php echo htmlspecialchars($data['task_no']); ?></td>
+                                <td><?php echo htmlspecialchars($data['task_date']); ?></td>
+                                <td><?php echo htmlspecialchars($data['category_no']); ?></td>
+                                <td><?php echo htmlspecialchars($data['task_title']); ?></td>
+                                <td>
+                                    <input type="checkbox" <?php echo $data['is_com'] == '1' ? 'checked' : ''; ?> onclick="if(this.checked){this.parentNode.parentNode.style.textDecoration='line-through';}else{this.parentNode.parentNode.style.textDecoration='none';}">
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            <!-- 페이지네이션 출력 -->
+            <ul class="paging-wrap">
+    
+                <?php
+                $prevPage = ($current_page_no == 1) ? $total_page_count : $current_page_no - 1;
+                $nextPage = ($current_page_no == $total_page_count) ? 1 : $current_page_no + 1;
+                if ($current_page_no > 1) {
+                ?>
+                    <li><a href='index.php?page=<?php echo $prevPage; ?>'>Prev</a></li>
+                <?php } ?>
+                <?php
+                for ($i = 1; $i <= $total_page_count; $i++) {
+                    if ($i === (int)$current_page_no) {
+                ?>
+                        <li><a href="/project/index.php?page=<?php echo $i ?>" class="page-icon active"><?php echo $i ?></a></li>
+                    <?php
+                    } else {
+                    ?>
+                        <li><a href="/project/index.php?page=<?php echo $i ?>" class="page-icon"><?php echo $i ?></a></li>
+                    <?php
+                    }
+                }
+                if ((int)$current_page_no < $total_page_count) {
+                    ?>
+                    <li><a href='/project/index.php?page=<?php echo $nextPage; ?>'>Next</a></li>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
