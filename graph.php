015077d0543367d9ff7d55dba649afdd0001d8ec
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-04-20 14:44:27 +0900

    pro

--- graph.php
+++ graph.php
@@ -8,9 +8,9 @@ $conn = get_db_conn();
 
 
 // 최근 일주일간의 기상카테고리 시작시간 평균 구하는 쿼리
-$sql1 = "SELECT AVG(TIME_TO_SEC(start_time)) AS avg_start_time
+$sql1 = "SELECT Floor(AVG(TIME_TO_SEC(start_time))) AS avg_start_time
         FROM Task
-        WHERE category_no = (SELECT category_no FROM Category WHERE category_name = '기상')
+        WHERE category_no = (SELECT category_no FROM Category WHERE category_name = '독서')
         AND task_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)";
 $result1 = $conn->query($sql1);
 $row1 = $result1->fetchAll();
@@ -26,7 +26,7 @@ $result2 = $conn->query($sql2);
 
 // 결과 출력
 foreach ($row1 as $avg) {
-    echo "최근 일주일간의 기상카테고리 시작시간 평균: " . gmdate("H:i:s", $avg["avg_start_time"]) . "<br><br>";
+    echo "최근 일주일간의 독서카테고리 시작시간 평균: " . gmdate("H:i:s", $avg["avg_start_time"]) . "<br><br>";
 }
 
 foreach ($result2 as $row2) {
