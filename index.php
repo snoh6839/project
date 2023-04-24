d1e6fae733320efc0443b909fe2f15aa7716dcf1
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-04-24 10:43:04 +0900

    update

---
+++ index.php
@@ -0,0 +1,192 @@
+<?php
+define("DOC_ROOT", $_SERVER["DOCUMENT_ROOT"] . "/");
+define("URL_DB", DOC_ROOT . "project/DB/db_conn.php");
+
+include_once(URL_DB);
+
+// DB 연결 객체 가져오기
+$db_conn = get_db_conn();
+if (!$db_conn) {
+    // DB 연결 실패 시, 예외 발생
+    throw new Exception("DB 연결에 실패했습니다.");
+}
+
+// 전체 데이터 수 가져오기
+$total_data_count = $db_conn->query('SELECT COUNT(*) FROM task')->fetchColumn();
+
+// task table과 category table을 별도로 조회한 후 PHP에서 조합하여 출력
+$sql = 'SELECT t.*, c.category_name FROM task t, category c WHERE t.category_no = c.category_no ORDER BY task_no DESC LIMIT :page_data_count OFFSET :start_index';
+$stmt = $db_conn->prepare($sql);
+$page_data_count = 5; // 페이지당 보여줄 데이터 수
+$total_page_count = ceil($total_data_count / $page_data_count);
+
+// 해당 페이지에 보여줄 데이터 구하기
+$current_page_no = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$current_page_no = max($current_page_no, 1); // 페이지 번호는 1 이상이어야 함
+$current_page_no = min($current_page_no, $total_page_count); // 페이지 번호는 전체 페이지 수 이하이어야 함
+$start_data_index = ($current_page_no - 1) * $page_data_count; // 페이지의 시작 데이터 인덱스
+$stmt->bindParam(':start_index', $start_data_index, PDO::PARAM_INT);
+$stmt->bindParam(':page_data_count', $page_data_count, PDO::PARAM_INT);
+$stmt->execute();
+$task_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//checked 시 수행 여부 업데이트
+function update_is_com($param_arr)
+{
+    $sql =
+        "UPDATE task
+        SET is_com = :is_com
+        WHERE task_no = :task_no";
+
+    $arr_prepare = array(
+        ":is_com" => $param_arr["is_com"],
+        ":task_no" => $param_arr["task_no"]
+    );
+
+    $db_conn = get_db_conn();
+
+    try {
+        $db_conn->beginTransaction();
+        $stmt = $db_conn->prepare($sql);
+        $stmt->execute($arr_prepare);
+        $result = $stmt->rowCount();
+        $db_conn->commit();
+    } catch (Exception $e) {
+        $db_conn->rollback();
+        return $e->getMessage();
+    } finally {
+        if ($db_conn !== null) {
+            $db_conn = null;
+        }
+    }
+    return $result;
+}
+
+
+
+
+$http_method = $_SERVER["REQUEST_METHOD"];
+// $db_conn = get_db_conn();
+
+if ($http_method === "POST") {
+    $arr_post = $_POST;
+    $test1 = isset($arr_post["is_com"]) ? $arr_post["is_com"] : 0;
+    $test2 = isset($arr_post["task_no"]) ? $arr_post["task_no"] : 0;
+    $is_com_old = array(
+        "is_com" => $test1,
+        "task_no" => $test2
+    );
+    $is_com = update_is_com($is_com_old);
+}
+
+
+// HTML 페이지에 표시할 코드 작성
+?>
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>미라클 모닝</title>
+    <!-- css 링크 -->
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+    <div class="sidebox">
+        <div class="top">
+            <h1>미라클 모닝 <span>실천방법</span></h1>
+        </div>
+        <div class="bottom">
+            <div class="update">
+                <p>1) <span>침묵의 시간</span> 갖기 - 삶의 목적 찾기<br>
+                    2) <span>확신</span>과 <span>다짐</span>의 말하기 - 잠재의식 프로그래밍<br>
+                    3) <span>직관</span>의 <span>시각화</span> - 이상적인 하루와 나의 모습<br>
+                    4) <span>아침 운동</span>하기 - 가장 좋아하고 몸 상태에 맞는<br>
+                    5) <span>독서</span>하기 - 목적 독서<br>
+                    6) <span>기록</span>하기 - 아침 일기</p>
+            </div>
+        </div>
+    </div>
+    <div class="contianer">
+        <div class="title top">
+            <h1><img src="./source/sun.png">  MIRACLE MORNING  <img src="./source/sun.png"></h1>    
+        </div>
+        <div class="bottom">
+            <div class="listTable">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>글번호</th>
+                            <th>날짜</th>
+                            <th>카테고리</th>
+                            <th>제목</th>
+                            <th>수행여부</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($task_data as $data) { ?>
+                            <tr <?php echo $data['is_com'] == '1' ? 'class="completed"' : '' ?>>
+
+                                <td><a href="/project/detail.php?task_no=<?php echo $data["task_no"] ?>"><?php echo $data['task_no'] ?></a></td>
+                                <td><a href="/project/detail.php?task_no=<?php echo $data["task_no"] ?>"><?php echo $data['task_date'] ?></a></td>
+                                <td><a href="/project/detail.php?task_no=<?php echo $data["task_no"] ?>"><?php echo $data['category_name'] ?></a></td>
+                                <td><a href="/project/detail.php?task_no=<?php echo $data["task_no"] ?>"><?php echo $data['task_title'] ?></a></td>
+
+                                <td>
+                                    <form action="" method="POST">
+                                        <input type="hidden" name="task_no" value="<?php echo $data['task_no'] ?>">
+                                        <?php if ($data['is_com'] == '1') { ?>
+                                            <button class="checkbox_btn_com"></button>
+                                        <?php } else { ?>
+                                            <button class="checkbox_btn"></button>
+                                        <?php } ?>
+                                        <input type="hidden" name="is_com" value="<?php echo $data['is_com'] == '1' ? '0' : '1' ?>">
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            <!-- 페이지네이션 출력 -->
+            <ul class="paging-wrap">
+                <li><a href="<?php echo $_SERVER['PHP_SELF'] . '?page=1' ?>">◀◀</a></li>
+                <?php
+                $start_page = floor(($current_page_no - 1) / 5) * 5 + 1; // 시작 페이지 계산
+                $end_page = $start_page + 4; // 끝 페이지 계산
+                $end_page = min($end_page, $total_page_count); // 끝 페이지가 전체 페이지 수보다 많으면 전체 페이지 수로 설정
+
+                if ($current_page_no >= 1) {
+                ?>
+                    <li><a href="<?php echo $_SERVER['PHP_SELF'] . '?page=' . ($current_page_no - 1); ?>">Prev</a></li>
+                <?php
+                }
+
+                for ($page_no = $start_page; $page_no <= $end_page; $page_no++) {
+                ?>
+                    <li <?php echo $page_no == $current_page_no ? 'class="active"' : ''; ?>>
+                        <a href="<?php echo $_SERVER['PHP_SELF'] . '?page=' . $page_no; ?>"><?php echo $page_no; ?></a>
+                    </li>
+                <?php
+                }
+
+                if ($end_page <= $total_page_count) {
+                ?>
+                    <li><a href="<?php echo $_SERVER['PHP_SELF'] . '?page=' . ($current_page_no + 1); ?>">Next</a></li>
+                <?php
+                }
+                ?>
+                <li><a href="<?php echo $_SERVER['PHP_SELF'] . '?page=' . ($total_page_count); ?>">▶▶</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="btn-wrap">
+        <a href="/project/graph.php" class="btn index2">분석</a>
+        <a href="/project/write.php" class="btn index1">추가</a>
+    </div>
+</body>
+
+</html>
---
+++ update.php
@@ -0,0 +1,135 @@
+<?php
+    define( "DOC_ROOT", $_SERVER["DOCUMENT_ROOT"]."/" ); // $_SERVER : 슈퍼글로벌 변수 ($_대문자) / 현재 사이트가 위치한 서버상의 위치
+    define( "URL_DB", DOC_ROOT."project/DB/db_conn.php" );
+    include_once( URL_DB );
+    
+    // Request Method를 획득
+    $http_method = $_SERVER["REQUEST_METHOD"];
+
+    // GET 일 때
+    if ( $http_method === "GET" )
+    {
+        $task_no = 1;
+        if( array_key_exists( "task_no", $_GET ) )
+        {
+            $task_no = $_GET["task_no"];
+        }
+        $result_info = select_task_info_no( $task_no );
+    }
+    // POST 일 때
+    else
+    {
+        $arr_post = $_POST;
+        $arr_info= 
+            array(
+                "task_no"       => $arr_post["task_no"]
+                ,"task_date"	=> $arr_post["task_date"]
+                ,"start_time"	=> $arr_post["start_time"]
+                ,"end_time" 	=> $arr_post["end_time"]
+                ,"task_title"	=> $arr_post["task_title"]
+                ,"is_com"		=> $arr_post["is_com"]
+                ,"task_memo"	=> $arr_post["task_memo"]
+                ,"category_no"	=> $arr_post["category_no"]
+            );
+            
+		// update
+		$result_cnt = update_task_info_no( $arr_info );
+
+        header( "Location: detail.php?task_no=".$arr_post["task_no"] );
+        exit(); // 36행에서 redirect 했기 때문에 이후의 소스코드는 실행할 필요가 없다.
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>게시글 수정페이지</title>
+    <link href="./css/main.css" rel="stylesheet" type="text/css">
+    <link href="./css/detail.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="sidebox">
+        <div class="top"></div>
+        <div class="bottom">
+            <div class="update">
+                매일 <span>아침</span> 눈뜨며 생각하자.<br><br>
+                오늘 <span>아침</span> 일어날 수 있으니<br>
+                이 얼마나 <span>행운</span>인가.<br><br>
+                나는 살아있고 소중한 <span>인생</span>을 가졌으니<br>
+                낭비하지 않을 것이다.<br><br>
+                나는 스스로를 <span>발전</span>시키고<br>
+                타인에게 나의 마음을 <span>확장</span>시켜<br>
+                나가기 위해 모든 기운을 쏟을 것이다.<br><br>
+                내 힘이 닿는 데까지<span>타인</span>을 이롭게 할 것이다.
+            </div>  
+        </div>
+    </div>
+    <div class="contianer">
+        <div class="title top">
+        <form method="post" action="update.php" id="form">
+            <input type="hidden" value="<?php echo $result_info["task_no"] ?>" name="task_no">
+            <label for="date"></label><img src="./source/sun.png">&nbsp;&nbsp;
+            <input type="date" value="<?php echo $result_info["task_date"] ?>" name="task_date" reqired>&nbsp;&nbsp;<img src="./source/sun.png">
+        </div>
+        <div class="bottom">
+            <div class="listTable">
+                <ul>
+                    <li>
+                        <label for="start_time">시작시간 </label>
+                        <input type="time" value="<?php echo $result_info["start_time"] ?>" name="start_time" reqired>
+                    </li>
+                    <li>
+                        <label for="end_time">종료시간 </label>
+                        <input type="time" value="<?php echo $result_info["end_time"] ?>" name="end_time" reqired>
+                    </li>
+                    <li>
+                        <label for="category">카테고리 
+                        <select name = "category_no" reqired>
+                        <option value= 1>독서</option>
+                        <option value= 2>운동</option>
+                        <option value= 3>공부</option>
+                        <option value= 4>기상</option>
+                        <option value= 5>취미</option>
+                        <option value= 6>회의</option>
+                        <option value= 7>쇼핑</option>
+                        <option value= 8>요리</option>
+                        <option value= 9>청소</option>
+                        <option value= 10>친구</option>
+                        <option value= 11>가족</option>
+                        <option value= 12>여행</option>
+                        <option value= 13>영화</option>
+                        <option value= 14>휴식</option>
+                        <option value= 15>기타</option>
+                        <option value= 16>병원</option>
+                        <option value= 17>식사</option>
+                        </select>
+                        </label>
+                    </li>
+                    <li>
+                        <label for="title">제목 </label>
+                        <input type="text" value="<?php echo $result_info["task_title"] ?>" name="task_title" id="title" reqired>
+                    </li>
+                    <li>
+                        <label for="complete">수행여부 완료</label>
+                        <input type="hidden" name="is_com" value="0">
+                        <input type="checkbox" name="is_com" value="1" <?php echo $result_info['is_com'] == '1' ? 'checked' : '' ?> id="complete">
+                    </li>
+                    <li>
+                        <label for="memo">메모 </label>
+                        <input type="text" value="<?php echo $result_info["task_memo"] ?>" name="task_memo" id="memo">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="btn-wrap">
+        <button type="button" onclick="location.href='index.php'" class="btn index2">리스트</button>
+        <button type="submit" class="btn index1">저장</button>
+    </div>
+</form>
+
+</body>
+</html>
